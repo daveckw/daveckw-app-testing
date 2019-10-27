ae2cc148f8e45f7520ea2101a5662a144f54af17
daveckw@gmail.com <daveckw@gmail.com>
2019-10-27 12:31:02 +0700

    Rerender after saving data

--- src/components/table/table.components.jsx
+++ src/components/table/table.components.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import "./table.styles.scss"
 
 const TableComponent = ({users, name, email, phoneNumber})=>(
-    <div>
-        <h2>{name} : {email} : {phoneNumber}</h2>
-        <ul>
+    <div className='myDiv'>
+       
+        <ul className="myList-Group">
             {users.map(printOut)}
         </ul>
+        <h2>{name} : {email} : {phoneNumber}</h2>
     </div> 
 )
 
 function printOut(user){
     return (
-        <li>{user.name} : {user.email} : {user.phoneNumber}</li>
+        <li className="myList-Item">{user.name} : {user.email} : {user.phoneNumber}</li>
     )
 }
 
